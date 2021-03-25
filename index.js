0b2e24701bc569d009b620c3d63ae071babfe32a
Henrique Barros <henriquesbarroshs@gmail.com>
2021-03-25 10:53:26 -0300

    objetos

---
+++ index.js
@@ -0,0 +1,45 @@
+const nomePetshop = 'PETSHOP AVANADE';
+
+let pets = [
+    {
+        nome: 'Costelinha',
+        tipo: 'cachorro',
+        idade: 5,
+        raca: 'Vira-lata',
+        peso: 3,
+        tutor: 'Doug',
+        contato: '(81) 98774-1992',
+        vacinado: true,
+        servicos: ['banho', 'tosa']
+    },
+    {
+        nome: 'Rufos',
+        tipo: 'Hamster',
+        idade: 1,
+        raca: null,
+        peso: '450g',
+        tutor: 'Henrique',
+        contato: '(81) 98774-1992',
+        vacinado: false,
+        servicos: ['banho']
+    },
+    {
+        nome: 'Scooby',
+        tipo: 'Pastor alemão',
+        idade: 6,
+        raca: null,
+        peso: 4,
+        tutor: 'Henrique',
+        contato: '(81) 98774-1992',
+        vacinado: false,
+        servicos: ['banho']
+    }
+]
+
+const listarPets = () => {
+    for (let i = 0; i < pets.length; i++) {
+        console.log(pets[i].nome)
+    }
+}
+
+listarPets()
